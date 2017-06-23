e76c3ff2e6d092098ff74d1f1471bffbcc62c1d8
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2017-06-23 13:04:49 -0700

    Function level scope

---
+++ variableInsideFunction.js
@@ -0,0 +1,11 @@
+function setVar() {
+	// New scope. 
+
+	// We can't acces to this variable outside this scope.
+	var myName = 'Cristina Rojas';
+}
+
+setVar();
+
+// We can't acces to the function scope (inside scop
+console.log(myName); e). // myName is not definded.
